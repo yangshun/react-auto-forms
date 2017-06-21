2302b96b5220386c4fc145ed54691b03aeb5c272
Tay Yang Shun <tay.yang.shun@gmail.com>
2017-06-22 01:06:08 +0800

    Smarter splitting that handles trailing whitespaces

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 export function classNameList(className) {
-  return className.split(' ').filter(str => str);
+  return className.trim().split(/\s+/);
 }
